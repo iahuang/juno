ab9afae860502a2b8bf6d4c7bc3331ca188212e4
Ian Huang <iahuang@users.noreply.github.com>
2023-03-16 00:44:38 -0400

    Fix borrow bug

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,15 @@ fn main() {
     let layout = vm::MemoryLayout::mars(0x1000, 0x1000);
     let mut vm = vm::VM::new(layout);
 
+    {
+        let stack = vm.memory.segment_by_name("stack").unwrap();
+        vm.memory.set_word(stack.get_high_address() - 3, 0xffeeddcc);
+    }
+
     let stack = vm.memory.segment_by_name("stack").unwrap();
 
-    vm.memory.set_word(stack.get_high_address() + 4, 329488293);
-
     // print some memory
-    for addr in stack.get_high_address() - 4..stack.get_high_address() {
-        println!("{:#010x}: {:#010x}", addr, vm.memory.get_word(addr));
+    for addr in stack.get_high_address() - 3..stack.get_high_address()+1 {
+        println!("{:#010x}: {}", addr, vm.memory.get_byte(addr));
     }
 }
